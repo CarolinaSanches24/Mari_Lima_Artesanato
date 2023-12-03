a2c1594e87d89d8174d2d5fb7a989abc598533db
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-12-03 03:16:01 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ const swiper = new Swiper(".swiper", {
     type: "bullets",
   },
 });
+
+document.getElementById("btn_cadastrar").addEventListener("click", function () {
+  const containerLogin = document.querySelector(".container_login");
+  containerLogin.style.display = "none";
+  const containerCadastro = document.querySelector(".container_cadastro");
+  containerCadastro.style.display = "block";
+});
+document.getElementById("btn_logar").addEventListener("click", function () {
+  const containerCadastro = document.querySelector(".container_cadastro");
+  containerCadastro.style.display = "none";
+});
+const containerLogin = document.querySelector(".container_login");
+containerCadastro.style.display = "block";
